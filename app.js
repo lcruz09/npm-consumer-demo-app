325e298f21ef157e7bd1848b4bb573020cc158d2
lcruz <lcruz@intelligensense.com>
2016-01-13 15:04:20 -0600

    Added npm example

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var producer = require('npm-producer-demo-app');
-producer.testModule();
+/**
+ * Copyright 2009 Intelligent Sense.
+ * All Rights Reserved.
+ * 
+ * This software is the propietary information of 
+ * Intelligent Sense.
+ * Use this subject to license terms.
+ *
+ * Filename: app.js
+ */
+
+
+// Load the git module
+var gitProducer = require('git-producer-demo-app');
+
+// Load the npm module 
+var npmProducer = require('npm-producer-demo-app');
+
+// Test the modules
+gitProducer.testModule();
+npmProducer.testModule();
